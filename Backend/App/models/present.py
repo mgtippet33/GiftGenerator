a24a1aea373e0b24374058306e2b666a904a5ba4
MagisterFelix <felix.vlad.14@gmail.com>
2021-05-21 12:39:53 +0300

    Add rating field

--- Backend/App/models/present.py
+++ Backend/App/models/present.py
@@ -9,6 +9,7 @@ class Present(models.Model):
     link = models.CharField(max_length=200, unique=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     desc = models.CharField(max_length=400, blank=True)
+    rate = models.IntegerField(default=0)
     criteria = models.ManyToManyField(Criterion)
     holidays = models.ManyToManyField(Holiday)
 
